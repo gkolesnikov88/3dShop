89a2cc2dbb6494bb2ac9e36a5514d830b9aa2b85
George Kolesnikov <gkolesnikov88@gmail.com>
2020-12-07 17:43:04 +0300

    Added frontend authentication and profile screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,32 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  userUpdateProfileReducer
+} from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems") ?
   JSON.parse(localStorage.getItem("cartItems")) : [];
 
+const userInfoFromLocalStorage = localStorage.getItem("userInfo") ?
+  JSON.parse(localStorage.getItem("userInfo")) : null;
+
 const initialState = {
-  cart: { cartItems: cartItemsFromLocalStorage}
+  cart: { cartItems: cartItemsFromLocalStorage },
+  userLogin: { userInfo: userInfoFromLocalStorage }
 };
 
 const middleware = [thunk];
